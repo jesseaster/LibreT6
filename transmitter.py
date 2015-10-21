d7f3a46cb18e08edde82c1c305661e8767ef4179
Justin Easter <jmeaste3@ncsu.edu>
2015-10-21 02:57:22 -0400

    Create list with channel values

--- transmitter.py
+++ transmitter.py
@@ -3,36 +3,51 @@ import serial
 import io
 import time
 
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0, xonxoff=True)
+def main():
+    ser = openSerial()
+    getChannelPositions(ser)
+    print(ser.close())
 
-print("Serial open:", ser.isOpen())
-send = bytearray.fromhex('55fd')
-print(send)
-ser.write(send)
-while ser.inWaiting() == 0:
-    x=2
-bytesToRead = ser.inWaiting()
-print(bytesToRead)
-print(ser.read(bytesToRead))
-while ser.inWaiting() == 0:
-    x=2
-bytesToRead = ser.inWaiting()
-print(bytesToRead)
-print(ser.read(bytesToRead))
-exit = 0
-ch1 = 0
-ch2 = 0
-ch3 = 0
-ch4 = 0
-ch5 = 0
-ch6 = 0
-while exit == 0:
+def openSerial():
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0, xonxoff=True)
+    return ser
+
+def getChannelPositions(ser):
+    print("Serial open:", ser.isOpen())
+    send = bytearray.fromhex('55fd')
+    print(send)
+    ser.write(send)
     while ser.inWaiting() == 0:
-        #do nothing
-        x=2
+        time.sleep(.007)
     bytesToRead = ser.inWaiting()
-    read = ser.read(bytesToRead)
-    for i in read:
-        print("{0:9b}".format(i), end = '')
-    print("")
-print(ser.close())
+    ser.read(bytesToRead)
+    while ser.inWaiting() == 0:
+        time.sleep(.007)
+    bytesToRead = ser.inWaiting()
+    ser.read(bytesToRead)
+    exit = 0
+
+    # infinite loop
+    while exit == 0:
+        while ser.inWaiting() == 0:
+            time.sleep(.007)
+        bytesToRead = ser.inWaiting()
+
+        # Sometimes only 17 bytes are sent with a byte missing from channel 4
+        if bytesToRead != 18:
+            ser.flushInput()
+        else:
+            read = ser.read(bytesToRead)
+            data = 0
+            for i in read:
+                data = (data<<8) + i
+            channels = []
+            while data !=0:
+                channels.append(data & 65535)
+                data = data >> 16
+            channels.reverse()
+
+            print(channels)
+
+if __name__ == "__main__":
+    main()
